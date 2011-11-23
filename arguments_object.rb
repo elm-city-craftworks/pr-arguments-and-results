6c11b879e14b301a9b0ae8b115fce8d02cd2be43
Gregory Brown <gregory.t.brown@gmail.com>
2011-11-22 20:55:20 -0500

    consistency

--- arguments_object.rb
+++ arguments_object.rb
@@ -1,6 +1,43 @@
 require "builder"
 
 class Drawing
+  def initialize(width, height)
+    @width  = width
+    @height = height
+
+    @viewbox_width  = (@width  * 100).ceil
+    @viewbox_height = (@height * 100).ceil
+
+    @lines  = []
+  end
+
+  def line(data, style)
+    @lines << { :x1 => data[0].x.to_s, :y1 => data[0].y.to_s, 
+                :x2 => data[1].x.to_s, :y2 => data[1].y.to_s,
+                :stroke => style.stroke_color, :"stroke-width" => style.stroke_width } 
+  end
+
+  attr_reader :width, :height, :lines
+
+  def to_svg
+    builder = Builder::XmlMarkup.new(:indent => 2)
+    builder.instruct!
+    builder.declare!(:DOCTYPE, :svg, :PUBLIC, "-//W3C//DTD SVG 1.1//EN",
+                     "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd")
+
+    svg_params = { :width   => "#{width}cm", 
+                   :height  => "#{height}cm",
+                   :viewBox => "0 0 #{@viewbox_width} #{@viewbox_height}",
+                   :xmlns   => "http://www.w3.org/2000/svg",
+                   :version => "1.1" }
+
+    builder.svg(svg_params) do |svg|
+      @lines.each { |params| svg.line(params) }
+    end
+
+    builder.target!
+  end
+
   class Point
     def initialize(x, y)
       @x, @y = x, y
@@ -26,40 +63,6 @@ class Drawing
     end
 
     attr_reader :stroke_width, :stroke_color
-  end
-
-  def initialize(width, height)
-    @width  = width
-    @height = height
-
-    @lines  = []
-  end
-
-  def line(data, style)
-    @lines << { :x1 => data[0].x.to_s, :y1 => data[0].y.to_s, 
-                :x2 => data[1].x.to_s, :y2 => data[1].y.to_s,
-                :stroke => style.stroke_color, :"stroke-width" => style.stroke_width } 
-  end
-
-  attr_reader :width, :height, :lines
-
-  def to_svg
-    builder = Builder::XmlMarkup.new(:indent => 2)
-    builder.instruct!
-    builder.declare!(:DOCTYPE, :svg, :PUBLIC, "-//W3C//DTD SVG 1.1//EN",
-                     "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd")
-
-    svg_params = { :width   => "#{width}cm", 
-                   :height  => "#{height}cm",
-                   :viewBox => "0 0 #{(width*100).to_i} #{(height*100).to_i}",
-                   :xmlns   => "http://www.w3.org/2000/svg",
-                   :version => "1.1" }
-
-    builder.svg(svg_params) do |svg|
-      @lines.each { |params| svg.line(params) }
-    end
-
-    builder.target!
   end
 end
 
